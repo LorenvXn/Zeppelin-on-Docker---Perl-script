3a59146cb5a5cc2e1dcb1928a24197df440ed32a
Satanette <predark.dante@gmail.com>
2017-01-27 18:10:33 +0200

    Update start_browser.sh

--- start_browser.sh
+++ start_browser.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-IP=`docker inspect $(docker ps | sed -e 's/^\(.\{10\}\).*/\1/' | awk 'NR==2')\
- | grep IPAddress | awk 'NR==1 {print $NF}' | cut -f1 -d ',' | \
-sed 's/["]//g'`
+IP=`docker inspect $(docker ps | sed -e 's/^\(.\{10\}\).*/\1/' | awk 'NR==2') \
+ | grep IPAddress | awk 'NR==1 {print $NF}' | cut -f1 -d ','  \
+ | sed 's/["]//g'`
+
 echo $IP
 
 python -m webbrowser http://$IP:8080
