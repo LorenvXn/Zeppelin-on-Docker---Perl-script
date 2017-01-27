2d41dc9724590da817b2bef56e4a330aed9f3d31
Satanette <predark.dante@gmail.com>
2017-01-27 14:47:40 +0200

    Update zepl.pl

--- zepl.pl
+++ zepl.pl
@@ -13,8 +13,9 @@ RUN  apt-get update \\
 print $fh "RUN apt-get install software-properties-common -y
 RUN add-apt-repository ppa:webupd8team/java -y
 RUN apt-get update
+RUN echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections";
 
-RUN echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
+print $fh "
 RUN apt-get install oracle-java7-installer -y";
 
 print $fh "
