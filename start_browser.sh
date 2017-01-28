0ca74115eaa29df4e88ba4a3a113a06447a5d083
Satanette <predark.dante@gmail.com>
2017-01-28 18:37:21 +0200

    Update start_browser.sh

--- start_browser.sh
+++ start_browser.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-IP=`docker inspect $(docker ps | sed -e 's/^\(.\{10\}\).*/\1/' | awk 'NR==2') \
+IP=`docker inspect $(docker ps  | sed -e 's/^\(.\{41\}\).*/\1/' | awk '{if($2 == "ledzeppelin:latest") print $1}') \
  | grep IPAddress | awk 'NR==1 {print $NF}' | cut -f1 -d ','  \
  | sed 's/["]//g'`
 
 echo $IP
 
 python -m webbrowser http://$IP:8080
+
