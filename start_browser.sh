4eee6ea657fdc0d59851b847135a2405408e586d
Satanette <predark.dante@gmail.com>
2017-02-03 14:41:33 +0200

    Update start_browser.sh

--- start_browser.sh
+++ start_browser.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IP=`docker inspect $(docker ps  | sed -e 's/^\(.\{41\}\).*/\1/' | awk '{if($2 == "ledzeppelin:latest") print $1}') \
- | grep IPAddress | awk 'NR==2 {print $NF}' | cut -f1 -d ','  \
+ | grep IPAddress | awk 'NR==1 {print $NF}' | cut -f1 -d ','  \
  | sed 's/["]//g'`
 
 echo $IP
