d1eae305c7cf6308d572e6772bf63de4e2aa5514
Satanette <predark.dante@gmail.com>
2017-02-24 13:57:36 +0200

    Update start_browser.sh

--- start_browser.sh
+++ start_browser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-IP=`docker inspect $(docker ps  | sed -e 's/^\(.\{41\}\).*/\1/' | awk '{if($2 == "ledzeppelin:latest") print $1}') \
+IP=`docker inspect $(docker ps  | sed -e 's/^\(.\{41\}\).*/\1/' | awk '{if($2 == "ledzeppelin") print $1}') \
  | grep IPAddress | awk 'NR==1 {print $NF}' | cut -f1 -d ','  \
  | sed 's/["]//g'`
 
@@ -8,3 +8,9 @@ echo $IP
 
 python -m webbrowser http://$IP:8080
 
+
+
+#################################################################################
+# if you get the "null" output at $IP, modify:  NR==2,
+#or simply check with docker inspect contaier ID and a grep on IPAddress the rows
+##################################################################################
